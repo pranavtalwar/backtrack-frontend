b65dee5718aadc203e35bc4bbad6f9302770c240
Rishabh <rhjain100@gmail.com>
2019-11-03 03:15:55 +0800

    Add PBI and create Tasks, started work

--- src/components/CreateSprint.js
+++ src/components/CreateSprint.js
@@ -1,6 +1,6 @@
 import React, { useState, useEffect } from 'react';
-import { Button, CssBaseline,Paper, 
-        InputLabel, TextField, MenuItem, Select, Container, Grid 
+import { Button, CssBaseline,Paper, Table, TableBody, TableRow, TableCell,
+        InputLabel, TextField, MenuItem, Select, Container, Grid, 
 } from '@material-ui/core';
 import {
     MuiPickersUtilsProvider,
@@ -40,6 +40,12 @@ const useStyles = makeStyles(theme => ({
   fixedHeight: {
     height: 240,
   },
+  newtext: {
+    fontSize: 25,
+  },
+  pbitext: {
+    fontSize: 18,
+  },
 }));
 
 export default () => {
@@ -47,8 +53,21 @@ export default () => {
   const [startDate, setStartDate] = useState(null);
   const [endDate, setEndDate] = useState(null);
   const [capacity, setCapacity] = useState(null);
-  const [currPBI, setCurrPBI] = useState(null);
   const [pbiArray, setPbiArray] = useState([]);
+  const [currPBI, setCurrPBI] = useState(null);
+  const [pbiTasks, setPBITasks] = useState([]);
+  const [isAdded, setIsAdded] = useState(false);
+  const [curTask, setCurTask] = useState({
+    description: null,
+    effort: null,
+  });
+
+  // const [addPbis, setAddPbis] = useState([]);
+  const hello = ({
+    id: 12,
+    name: "Make Soup",
+  });
+
   const url = "http://127.0.0.1:8000/pbi/";
 
   useEffect(() => {
@@ -56,6 +75,41 @@ export default () => {
     .then(response => response.json())
     .then(json => setPbiArray(json));
   }, []);
+
+  const handlePBIAdd = (e) => {
+    e.preventDefault();
+    console.log(currPBI);
+    if(currPBI!=null){
+      if(pbiTasks.some(pbiTask => pbiTask.id === currPBI.id)){
+        alert("Please select a different PBI")
+      } else {
+        setPBITasks([...pbiTasks, {
+          id: currPBI.id,
+          name: currPBI.name,
+          tasks: []
+        }]);
+      }
+    } else {
+      alert("Please select a PBI")
+    }
+    setIsAdded(true);
+    console.log(pbiTasks);
+  }
+
+  const handleTaskAdd = (e) => {
+    e.preventDefault();
+    const pbiIndex = pbiTasks.findIndex((pbiTask => pbiTask.id === currPBI.id));
+    console.log("Hello");
+    if(curTask.description!=null && curTask.effort!=null){
+      pbiTasks[pbiIndex].tasks.push(curTask);
+    } else if (curTask.description==null){
+      alert("Please enter task description")
+    } else {
+      alert("Please enter task effort")
+    }
+    console.log(pbiTasks);
+  }
+
   
   return (
     <div className={classes.root}>
@@ -106,11 +160,84 @@ export default () => {
             >
             {
                 pbiArray.map(pbi => (
-                  <MenuItem value={pbi.id}>{pbi.name}</MenuItem>
+                  <MenuItem value={pbi}>{pbi.name}</MenuItem>
                 ))
             }
+            <MenuItem value={hello}>Make Soup</MenuItem>
             </Select>
+            <Button
+              type="submit"
+              variant="contained"
+              color="primary"
+              onClick={handlePBIAdd}
+            >
+              Add
+            </Button>
         </form>
+        <br/>
+        <br/>
+          <div className={classes.pbitext}>
+          {isAdded? 
+            (
+              pbiTasks.map(row => (
+                <>
+                <div className={classes.newtext}>
+                Add Tasks: <br/>
+                </div>
+                <br/>
+                ID: {row.id}  Name: {row.name} <br/>
+                {
+                  (row.tasks.length>0)? (
+                    <Table>
+                      <TableBody>
+                        <TableRow>
+                          <TableCell>Description</TableCell>
+                          <TableCell>Effort</TableCell>
+                        </TableRow>
+
+                        {row.tasks.map(task => (
+                          <TableRow>
+                              <TableCell>{task.description}</TableCell>
+                              <TableCell>{task.effort}</TableCell>
+                          </TableRow>
+                        ))}
+                    </TableBody>
+                    </Table>
+                  ) : (
+                    <></>
+                  )
+                }
+                <form onSubmit={handleTaskAdd}>
+                  <TextField 
+                    multiline
+                    label="Description"
+                    onChange={e => setCurTask({ ...curTask, description: e.target.value })}
+                  />
+                  <TextField 
+                    multiline
+                    label="Effort"
+                    onChange={e => setCurTask({ ...curTask, effort: e.target.value })}
+                  />
+                  <Button
+                    type="submit"
+                    variant="contained"
+                    color="primary"
+                  >
+                    Add
+                  </Button>
+                </form>
+              </>
+              ))
+
+            ):
+            (
+              <center>Please Add PBI</center>
+            )
+          }
+        </div> 
+        <br/>
+        <br/>
+        <br/>
         <Copyright />   
       </div>
       
