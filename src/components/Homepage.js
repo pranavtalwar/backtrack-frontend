e3fc612b42d4b9c1546c26b58ade63d0718e470f
Pranav Talwar <talwar.pranav123@gmail.com>
2019-11-02 15:42:00 +0800

    fixed routing

--- src/components/Homepage.js
+++ src/components/Homepage.js
@@ -1,21 +1,9 @@
 import React from 'react';
 import { makeStyles } from '@material-ui/core/styles';
-import { Paper, Link, Grid, Container, Typography, CssBaseline} from '@material-ui/core';
+import { Paper, Grid, Container, Typography, CssBaseline} from '@material-ui/core';
 import {Button, Card} from '@material-ui/core';
 import AppBar from './AppBar';
-
-function Copyright() {
-  return (
-    <Typography variant="body2" color="textSecondary" align="center">
-      {'Copyright © '}
-      <Link color="inherit" href="https://material-ui.com/">
-        Your Website
-      </Link>{' '}
-      {new Date().getFullYear()}
-      {'.'}
-    </Typography>
-  );
-}
+import Copyright from './Copyright';
 
 const useStyles = makeStyles(theme => ({
   root: {
--- src/components/listItems.js
+++ src/components/listItems.js
@@ -2,37 +2,65 @@ import React from 'react';
 import { ListItem, ListItemText, ListItemIcon } from '@material-ui/core';
 import DashboardIcon from '@material-ui/icons/Dashboard';
 import { DoubleArrow, Create, Announcement } from '@material-ui/icons';
-import {Link} from 'react-router-dom';
+import { NavLink } from 'react-router-dom';
 
 export const mainListItems = (
   <div>
-  <Link to='/homepage'>
-    <ListItem button>
-      <ListItemIcon>
-        <DashboardIcon />
-      </ListItemIcon>
-      <ListItemText primary="Dashboard" />
-    </ListItem>
-    </Link>
-    <ListItem button>
-      <ListItemIcon>
-        <DoubleArrow />
-      </ListItemIcon>
-      <ListItemText primary="Current Sprint" />
-    </ListItem>
+    <NavLink 
+      to='/homepage' 
+      style={{ textDecoration: 'none', color: 'black' }} 
+      activeStyle={{
+        fontWeight: "bold",
+        color: "blue"
+    }}>
+      <ListItem button>
+        <ListItemIcon>
+          <DashboardIcon />
+        </ListItemIcon>
+        <ListItemText primary="Dashboard" />
+      </ListItem>
+    </NavLink>
+    <NavLink 
+      to='/currentsprint' 
+      style={{ textDecoration: 'none', color: 'black' }} 
+      activeStyle={{
+        fontWeight: "bold",
+        color: "blue"
+    }}>
+      <ListItem button>
+        <ListItemIcon>
+          <DoubleArrow />
+        </ListItemIcon>
+        <ListItemText primary="Current Sprint" />
+      </ListItem>
+    </NavLink>
+    <NavLink 
+      to='/createsprint' 
+      style={{ textDecoration: 'none', color: 'black' }} 
+      activeStyle={{
+        fontWeight: "bold",
+        color: "blue"
+    }}>
     <ListItem button>
       <ListItemIcon>
         <Create />
       </ListItemIcon>
       <ListItemText primary="Create Sprint" />
     </ListItem>
-    <Link to='/backlog'>
-    <ListItem button>
-      <ListItemIcon>
-        <Announcement />
-      </ListItemIcon>
-      <ListItemText primary="Backlog" />
-    </ListItem>
-    </Link>
+    </NavLink>
+    <NavLink 
+      to='/backlog' 
+      style={{ textDecoration: 'none', color: 'black' }} 
+      activeStyle={{
+        fontWeight: "bold",
+        color: "blue"
+    }}>
+      <ListItem button>
+        <ListItemIcon>
+          <Announcement />
+        </ListItemIcon>
+        <ListItemText primary="Backlog" />
+      </ListItem>
+    </NavLink>
   </div>
 );
